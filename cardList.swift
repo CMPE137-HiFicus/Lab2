2053a5e2f5f3218aa95b2cd12df8eb267af36cde
khamtran411 <44482407+khamtran411@users.noreply.github.com>
2019-09-11 08:33:31 -0700

    Add files via upload

--- cardList.swift
+++ cardList.swift
@@ -26,17 +26,45 @@ class cardList
                 {
                     for shading in cardShade.allValues
                     {
-                        let card = Card(cardColor:color,cardSymbol:symbol,cardNumber: number,cardShade: shading)
+                        let card = Card(idNum: 0, cardColor:color,cardSymbol:symbol,cardNumber: number,cardShade: shading)
                         list.append(card)
                     }
                 }
             }
         }
       }
+    func differentColor(set: [Card])-> Bool{
+        if set[0].cardColor != set[1].cardColor && set[0].cardColor != set[2].cardColor && set[1].cardColor != set[2].cardColor { return true}
+        else {return false}
+    }
     
+    func differentShade(set:[Card]) -> Bool {
+        if set[0].cardShade != set[1].cardShade && set[0].cardShade != set[2].cardShade && set[1].cardShade != set[2].cardShade { return true}
+        else {return false}
+    }
+    
+    func differentNumber(set: [Card])-> Bool {
+        if set[0].cardNumber != set[1].cardNumber && set[0].cardNumber != set[2].cardNumber && set[1].cardNumber != set[2].cardNumber { return true}
+        else {return false}
+    }
+    
+    func differenSymbol(set: [Card]) -> Bool {
+        if set[0].cardSymbol != set[1].cardSymbol && set[0].cardSymbol != set[2].cardSymbol && set[1].cardSymbol != set[2].cardSymbol{ return true}
+        else {return false}
+    }
+    
+    func isSet(set:[Card]) -> Bool {
+        if differenSymbol(set: set) && !(differentColor(set: set)) {return true}
+        
+        else { return false}
+    }
     
     func getCard(index:Int)->Card
     {
         return list[index]
+    }
+    func deleteCard(index:Int)
+    {
+        list.remove(at: index)
     }
 }
