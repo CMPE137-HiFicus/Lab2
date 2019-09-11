4e49c7ef93d7e5bb8e247db212566795ba56504f
khamtran411 <44482407+khamtran411@users.noreply.github.com>
2019-09-10 21:27:56 -0700

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -16,22 +16,39 @@ class ViewController: UIViewController {
     
     @IBAction func touchCards(_ sender:UIButton){
         
-         print("\(cardlist.list.count)")
+        print("\(cardlist.list.count)")
         updateViewFormModel()
-        
     }
-
-    func updateViewFormModel(){
-     
-        
+    @IBAction func start()
+    {
         for index in 0..<12
         {
             let Button = cardButtons[index]
             let card = cardlist.list[index]
             let temp:drawCard = drawCard()
             temp.renderCard(card: card, button: Button)
-    
+            cardlist.deleteCard(index: index)
         }
+    }
+    @IBAction func add3More()
+    {
+        var index = 0
+        for button in cardButtons
+        {
+            if (button.attributedTitle(for: UIControl.State.normal) == nil) && index < 3
+            {
+                let card = cardlist.list[index]
+                let temp:drawCard = drawCard()
+                temp.renderCard(card: card, button: button)
+                cardlist.deleteCard(index: index)
+                index += 1
+            }
+        }
+    }
+    func updateViewFormModel(){
+     
+        
+       
         
     }
     
